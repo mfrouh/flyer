f4f0a9989550b4be74284ec4ddb8e158fed2c85a
mohamed frouh <mohamedfrouh96@gmail.com>
2020-01-26 00:08:25 +0200

    first commit

--- resources/views/message/message.blade.php
+++ resources/views/message/message.blade.php
@@ -1,10 +1,10 @@
 @if(session('success'))
 <div class="alert alert-success alert-dismissible fade show" role="alert">
-    <button type="button" class="close l" data-dismiss="alert" aria-label="Close">
+    <button type="button" class="close " data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
         <span class="sr-only">Close</span>
     </button>
-    <strong>{{session('success')}}</strong>
+    <strong style="float: right;">{{session('success')}}</strong>
 </div>
 @endif
 
@@ -14,18 +14,18 @@
         <span aria-hidden="true">&times;</span>
         <span class="sr-only">Close</span>
     </button>
-    <strong>{{session('error')}}</strong>
+    <strong style="float: right;">{{session('error')}}</strong>
 </div>
 @endif
 
 @if(session('errors'))
 @foreach (session('errors')->all() as $error)
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
+<div class="alert alert-danger alert-dismissible fade show bg-danger" role="alert">
     <button type="button" class="close l" data-dismiss="alert" aria-label="Close" >
         <span aria-hidden="true">&times;</span>
         <span class="sr-only">Close</span>
     </button>
-    <strong>{{$error}}</strong>
+    <strong style="float: right;">{{$error}}</strong>
 </div>
 @endforeach
 @endif
