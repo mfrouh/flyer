05297bbb9e2b5d069e96997870df5ee82621929b
mohamed frouh <mohamedfrouh96@gmail.com>
2020-01-26 18:57:02 +0200

    first commit

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -72,25 +72,7 @@
             </div>
         </div>
         <div class="col-lg-9">
-            <div class="row">
-              @forelse ($flyers as $flyer)
-               <div class="col-lg-4 mb-5">
-                   <div class="card border-primary  shadow-lg">
-                     <a href="/flyer/{{$flyer->id}}">
-                        <img class="card-img-top" src="{{url('/storage/flyer')}}/{{json_decode($flyer->image)[0]}}"  height="200px">
-                     </a>
-                     <div class="card-footer bg-primary  text-right" style="font-size: smaller;">
-                       <label>السعر:</label> {{$flyer->price}}  <label>جنية</label><br>
-                       <label>المساحة:</label> {{$flyer->area}}  <label>متر</label><br>
-                       <label>نوع العقار:</label> {{$flyer->category->name}}
-                     </div>
-                   </div>
-               </div>
-              @empty
-              @include('message.notfound')
-              @endforelse
-            </div>
-            {{$flyers->links()}}
+            @include('pages.flyers')
         </div>
     </div>
 </div>
